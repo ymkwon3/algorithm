92437aecb0ad91d3ef1287fd17f5631cb742f7cc
youngmin <ymkwon3@gnu.ac.kr>
2021-03-21 02:34:25 +0900

    2021-03-20

--- c++/1504_specificShortestPath.cpp
+++ c++/1504_specificShortestPath.cpp
@@ -1,11 +1,66 @@
+/*
+
+1 -> v1 -> v2 -> n
+1 -> v2 -> v1 -> n
+
+*/
+
 #include <iostream>
 #include <vector>
 #include <queue>
+#define MAX 801
 using namespace std;
 
-vector<pair<int, int>> v[801];
-
+vector<pair<int, int>> v[MAX];
+int node[MAX];
 int n, e;
+int v1, v2;
+bool connect = true;
+
+int min(int a, int b)
+{
+    return a > b ? b : a;
+}
+
+void reset()
+{
+    for (int i = 0; i < MAX; i++)
+        node[i] = 0;
+}
+
+int dijkstra(int start, int end)
+{
+    if (start == end)
+    {
+        return 0;
+    }
+    else
+    {
+        reset();
+        priority_queue<pair<int, int>> pq; // <node, cost>
+        pq.push({start, 0});
+        while (!pq.empty())
+        {
+            int cur_node = pq.top().first;
+            int cur_cost = -pq.top().second;
+            pq.pop();
+            for (int i = 0; i < v[cur_node].size(); i++)
+            {
+                int next_node = v[cur_node][i].first;
+                int next_cost = v[cur_node][i].second;
+                if (node[next_node] == 0 || node[next_node] > cur_cost + next_cost)
+                {
+                    node[next_node] = cur_cost + next_cost;
+                    pq.push({next_node, -node[next_node]});
+                }
+            }
+        }
+        if (node[end] == 0)
+            connect = false;
+
+        return node[end];
+    }
+}
 
 int main(void)
 {
@@ -18,7 +73,21 @@ int main(void)
     {
         cin >> a >> b >> c;
         v[a].push_back({b, c});
+        v[b].push_back({a, c});
     }
+    cin >> v1 >> v2;
 
+    int d1, d2, d3;
+    int D1, D2, D3;
+    d1 = dijkstra(1, v1);
+    d2 = dijkstra(v1, v2);
+    d3 = dijkstra(v2, n);
+    D1 = dijkstra(1, v2);
+    D2 = dijkstra(v2, v1);
+    D3 = dijkstra(v1, n);
+    if (!connect)
+        cout << -1 << "\n";
+    else
+        cout << min(d1 + d2 + d3, D1 + D2 + D3) << "\n";
     return 0;
 }
