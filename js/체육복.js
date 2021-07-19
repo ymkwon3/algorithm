4b3c145099ce6dce9b59bc7b859a68c53e21d527
youngmin <ymkwon3@gnu.ac.kr>
2021-07-19 15:30:35 +0900

    2021-07-19-프로그래머스-체육복

---
+++ js/체육복.js
@@ -0,0 +1,37 @@
+let n = 5;
+let lost = [2, 4];
+let reserve = [3];
+
+function solution(n, lost, reserve) {
+  let student = [-1];
+  var answer = 0;
+  for (let i = 1; i <= n; i++) {
+    student.push(1);
+  }
+  for (let val of lost) {
+    student[val]--;
+  }
+  for (let val of reserve) {
+    student[val]++;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (student[i] === 2) {
+      if (i - 1 > 0 && student[i - 1] === 0) {
+        student[i - 1]++;
+        student[i]--;
+      } else if (i + 1 < n && student[i + 1] === 0) {
+        student[i + 1]++;
+        student[i]--;
+      }
+    }
+  }
+
+  for (let val of student) {
+    if (val > 0) answer++;
+  }
+  console.log(student, answer);
+  return answer;
+}
+
+solution(n, lost, reserve);
