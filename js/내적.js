0556e40ed22ffef8694b6ae187d3c2a4af2d4970
youngmin <ymkwon3@gnu.ac.kr>
2021-07-19 19:00:57 +0900

    2021-07-19-프로그래머스-내적

---
+++ js/내적.js
@@ -0,0 +1,12 @@
+const a = [1, 2, 3, 4];
+const b = [-3, -1, 0, 2];
+
+function solution(a, b) {
+  var answer = 0;
+  for (let i in a) {
+    answer += a[i] * b[i];
+  }
+  console.log(answer);
+  return answer;
+}
+solution(a, b);
