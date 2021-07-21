b991e4de566e2d9a6a2e6a63fc3bc2484e1b459b
youngmin <ymkwon3@gnu.ac.kr>
2021-07-21 21:46:39 +0900

    2021-07-21-프로그래머스-같은숫자는싫어

---
+++ js/같은숫자는싫어.js
@@ -0,0 +1,15 @@
+const arr = [1, 1, 3, 3, 0, 1, 1];
+solution(arr);
+function solution(arr) {
+  var answer = [];
+  let pre = arr[0];
+  answer.push(pre);
+  for (let val of arr) {
+    if (val !== pre) {
+      answer.push(val);
+      pre = val;
+    }
+  }
+  console.log(answer);
+  return answer;
+}
