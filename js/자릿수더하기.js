88763d605b5428aa4d46e29d43158eb46ed95574
youngmin <ymkwon3@gnu.ac.kr>
2021-07-22 21:43:08 +0900

    2021-07-22-프로그래머스-핸드폰번호가리기

--- js/자릿수더하기.js
+++ js/자릿수더하기.js
@@ -6,11 +6,9 @@ function solution(n) {
   var answer = 0;
   while (true) {
     answer += n % 10;
-    console.log(answer);
     n = Math.floor(n / 10);
     if (n < 1) break;
   }
-
   console.log(answer);
   return answer;
 }
