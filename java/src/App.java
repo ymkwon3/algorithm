e5225634802acd01f9b68bd86b5e2314b61077e5
youngmin <ymkwon3@gnu.ac.kr>
2021-07-28 21:03:29 +0900

    2021-07-28-프로그래머스-기능개발

--- java/src/App.java
+++ java/src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        더맵게 sol = new 더맵게();
-        int[] scoville = { 1, 2, 3 };
-        int K = 100;
-        sol.solution(scoville, K);
+        기능개발 sol = new 기능개발();
+        int[] progresses = { 93, 30, 55 };
+        int[] speeds = { 1, 30, 5 };
+        sol.solution(progresses, speeds);
     }
 }
