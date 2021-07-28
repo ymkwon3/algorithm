a9e532136d612be2b21f7d3f0fc42158acc3208a
youngmin <ymkwon3@gnu.ac.kr>
2021-07-28 11:16:08 +0900

    2021-07-28-프로그래머스-더맵게
    start with java

---
+++ java/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        더맵게 sol = new 더맵게();
+        int[] scoville = { 1, 2, 3 };
+        int K = 100;
+        sol.solution(scoville, K);
+    }
+}
