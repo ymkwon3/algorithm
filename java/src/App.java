a78abba3903a665621dad7d57e742f5a00de5080
youngmin <ymkwon3@gnu.ac.kr>
2021-07-30 15:56:38 +0900

    2021-07-30-프로그래머스-짝지어제거하기
    used stack with java

--- java/src/App.java
+++ java/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        가장큰수 sol = new 가장큰수();
-        int[] numbers = { 41, 412 };
-        sol.solution(numbers);
+        짝지어제거하기 sol = new 짝지어제거하기();
+        String s = "baabaa";
+        sol.solution(s);
     }
 }
---
+++ java/src/짝지어제거하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class 짝지어제거하기 {
+    public int solution(String s) {
+        int answer = -1;
+        Stack<Character> stack = new Stack<Character>();
+        stack.push(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            System.out.println(" size : " + stack.size());
+            if (!stack.empty() && s.charAt(i) == stack.peek()) {
+                stack.pop();
+                continue;
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        if (stack.empty())
+            answer = 1;
+        else
+            answer = 0;
+        return answer;
+    }
+}
