4c533fe7a48f6916725aab088255cb447c8f172c
youngmin <ymkwon3@gnu.ac.kr>
2021-07-31 21:24:19 +0900

    2021-07-31-프로그래머스-N개의최소공배수

--- java/src/App.java
+++ java/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        피보나치수 sol = new 피보나치수();
-        int n = 20;
-        sol.solution(n);
+        N개의최소공배수 sol = new N개의최소공배수();
+        int[] arr = { 2, 6, 8, 14 };
+        sol.solution(arr);
     }
 }
