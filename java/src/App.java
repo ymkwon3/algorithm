e31ff2c7b12090c205dc2801126f280bca71786a
youngmin <ymkwon3@gnu.ac.kr>
2021-08-06 01:30:23 +0900

    2021-08-06-프로그래머스-조이스틱

--- java/src/App.java
+++ java/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        카펫 sol = new 카펫();
-        sol.solution(24, 24);
+        조이스틱 sol = new 조이스틱();
+        sol.solution("ABAAAAAAAAABB");
     }
 }
