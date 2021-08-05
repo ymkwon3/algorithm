7a902e1843fc92c133a595cd9576a2621ade1732
youngmin <ymkwon3@gnu.ac.kr>
2021-08-05 19:22:08 +0900

    2021-08-05-프로그래머스-다리를지나는트럭
    user queue like shift

--- java/src/App.java
+++ java/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        HIndex sol = new HIndex();
-        int[] c = { 5, 5, 5, 5 };
-        sol.solution(c);
+        다리를지나는트럭 sol = new 다리를지나는트럭();
+        int[] truck_weights = { 1, 2, 3, 4, 5 };
+        sol.solution(10, 10, truck_weights);
     }
 }
