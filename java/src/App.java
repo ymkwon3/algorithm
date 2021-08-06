0909c758c5a28420100f48de99a7178ed8f97683
youngmin <ymkwon3@gnu.ac.kr>
2021-08-06 22:32:11 +0900

    2021-08-06-프로그래머스-카카오프렌즈컬러링북
    used dfs

--- java/src/App.java
+++ java/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        조이스틱 sol = new 조이스틱();
-        sol.solution("ABAAAAAAAAABB");
+        카카오프렌즈컬러링북 sol = new 카카오프렌즈컬러링북();
+        int[][] picture = { { 0, 1, 0 }, { 1, 1, 0 }, { 0, 0, 0 } };
+        sol.solution(3, 3, picture);
     }
 }
