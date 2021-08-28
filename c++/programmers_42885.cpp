bd0de8faf20125a51f9c66ccc12e854c48ad51d1
youngmin <ymkwon3@gnu.ac.kr>
2021-08-29 01:18:08 +0900

    2021-08-29-프로그래머스-구명보트-Greedy

---
+++ c++/programmers_42885.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit)
+{
+    int answer = 0;
+    sort(people.begin(), people.end());
+
+    int tmp = 0;
+    for (int i = people.size() - 1, j = 0; i >= j; i--)
+    {
+        tmp += people[i];
+        if (tmp == limit)
+        {
+            tmp = 0;
+            answer++;
+        }
+        else if (tmp < limit)
+        {
+            while (true)
+            {
+                tmp += people[j];
+                if (tmp == limit)
+                {
+                    tmp = 0;
+                    answer++;
+                    j++;
+                    break;
+                }
+                else if (tmp > limit)
+                {
+                    tmp = 0;
+                    answer++;
+                    break;
+                }
+                j++;
+            }
+        }
+    }
+    cout << answer << "\n";
+    return answer;
+}
+
+int main(void)
+{
+    vector<int> p = {50, 70, 80};
+    solution(p, 100);
+    return 0;
+}
