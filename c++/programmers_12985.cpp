9303541fd94cc39a1871eb59bd0f904f79c38610
youngmin <ymkwon3@gnu.ac.kr>
2021-09-05 23:49:59 +0900

    2021-09-05-프로그래머스-예상대진표

---
+++ c++/programmers_12985.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 1;
+    if (a / 2 == b / 2)
+        answer++;
+    while (abs(a - b) != 1)
+    {
+        a = a % 2 == 0 ? a / 2 : a / 2 + 1;
+        b = b % 2 == 0 ? b / 2 : b / 2 + 1;
+        answer++;
+    }
+    cout << answer << "\n";
+    return answer;
+}
+
+int main()
+{
+    solution(8, 4, 5);
+    return 0;
+}
