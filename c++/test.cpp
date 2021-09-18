33b5ec9fe672694b3cee7700d7341638d068bb0e
youngmin <ymkwon3@gnu.ac.kr>
2021-09-18 21:00:53 +0900

    2021-09-18-프로그래머스

--- c++/test.cpp
+++ c++/test.cpp
@@ -9,21 +9,92 @@
 #include <set>
 
 using namespace std;
-int calc(string a, string b)
-{
-    int aH = stoi(a.substr(0, 2)), aM = stoi(a.substr(3, 2));
-    int bH = stoi(b.substr(0, 2)), bM = stoi(b.substr(3, 2));
-    if (bM > aM)
-    {
-        aH--;
-        aM += 60;
-    }
-    return (aH - bH) * 60 + (aM - bM);
-}
+
 int main(void)
 {
-    string s = "23:59 06:05 77:77";
-    s = s.substr(12);
-    cout << s;
+    vector<string> address = {"11.114.225.001"};
+    vector<string> result;
+    for (string s : address)
+    {
+        string cur = "";
+        string ans = "IPv4";
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == ':')
+            {
+                ans = "IPv6";
+                break;
+            }
+            if (s[i] == '.')
+            {
+                if (cur == "" || (cur[0] == '0' && cur[1] != '0' && cur.size() == 3) || stoi(cur) > 255)
+                {
+                    ans = "Neither";
+                    break;
+                }
+                else
+                    cur = "";
+            }
+            else
+            {
+                cur += s[i];
+            }
+            if (i == s.size() - 1)
+            {
+                if (cur == "" || (cur[0] == '0' && cur[1] != '0' && cur.size() == 3) || stoi(cur) > 255)
+                {
+                    ans = "Neither";
+                    break;
+                }
+                else
+                    cur = "";
+            }
+        }
+        if (ans != "IPv6")
+        {
+            result.push_back(ans);
+            continue;
+        }
+        cur = "";
+        int colons = 0;
+        bool skip = false;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == ':')
+            {
+                if (s[i + 1] == ':')
+                {
+                    colons++;
+                    skip = true;
+                }
+                colons++;
+                if (cur.size() > 4)
+                {
+                    ans = "Neither";
+                    break;
+                }
+                cur = "";
+            }
+            else
+            {
+                if ((s[i] >= 48 && s[i] <= 57) || (s[i] >= 97 && s[i] <= 102))
+                {
+                    cur += s[i];
+                }
+                else
+                {
+                    ans = "Neither";
+                    break;
+                }
+            }
+        }
+        if (colons > 7)
+            ans = "Neither";
+        else if (colons < 7 && ans == "IPv6")
+            ans = skip ? "IPv6" : "Neither";
+        result.push_back(ans);
+    }
+    for (string s : result)
+        cout << s << "\n";
     return 0;
 }
---
+++ js/test.js
@@ -0,0 +1,2 @@
+let url = "123456789";
+console.log(url.length.toString())
