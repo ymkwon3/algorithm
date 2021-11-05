7aad8eb1093d15c9b0b78e15e8c33d681918dacd
youngmin <ymkwon3@gnu.ac.kr>
2021-11-05 23:56:12 +0900

    2021-11-05-프로그래머스-87946
    used dfs

--- c++/programmers_87946.cpp
+++ c++/programmers_87946.cpp
@@ -1,14 +1,46 @@
-#include <iostream>
+#include <string>
 #include <vector>
+#include <iostream>
 using namespace std;
+
+int dfs(int n, int k, vector<vector<int>> dungeons, int c, vector<int> v)
+{
+    int result = c - 1;
+    v[n] = 1;
+    if (k >= dungeons[n][0])
+    {
+
+        k -= dungeons[n][1];
+        result++;
+        for (int i = 0; i < dungeons.size(); i++)
+        {
+            if (v[i] == 0)
+            {
+                int tmp = dfs(i, k, dungeons, c + 1, v);
+                if (tmp > result)
+                    result = tmp;
+            }
+        }
+    }
+    return result;
+}
+
+int solution(int k, vector<vector<int>> dungeons)
+{
+    int answer = 0;
+    vector<int> v(dungeons.size());
+    for (int i = 0; i < dungeons.size(); i++)
+    {
+        int tmp = dfs(i, k, dungeons, 1, v);
+        if (tmp > answer)
+            answer = tmp;
+    }
+    cout << answer << "\n";
+    return answer;
+}
 
 int main(void)
 {
-    vector<int> v(4);
-    int n;
-    cin >> n;
-    for (int i = 0; i < 4; i++)
-        cin >> v[i];
-
-        return 0;
+    solution(80, {{80, 20}, {50, 40}, {30, 10}});
+    return 0;
 }
