70d6556bdc34df2748f4142ec27c2e273449612e
ingmin <ymkwon3@gmail.com>
2022-06-22 23:22:09 +0900

    3xn타일링 실패코드

--- js/3xn타일링.js
+++ js/3xn타일링.js
@@ -1,5 +1,9 @@
 function solution(n) {
     let list = [0, 0, 3, 0, 11, 0];
+    if (n == 2)
+        return list[2];
+    if (n == 4)
+        return list[4];
     for (let i = 6; i <= n; i++) {
         if (i % 2 === 1) {
             list.push(0);
@@ -10,3 +14,11 @@ function solution(n) {
 
     return list[n];
 }
+
+solution(4);
+
+/* 
+홀수일경우는 무조건 0??
+무엇인가 규칙성이 있을 것 같다 dp로 풀 수 있을듯한?
+
+*/
