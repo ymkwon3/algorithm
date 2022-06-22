463266f030017a4a8fb7f05b4dd90b142643bd0b
ingmin <ymkwon3@gmail.com>
2022-06-23 01:13:40 +0900

    다른 분의 알파벳 밀기, 모음 대문자 도와주기

--- js/test.js
+++ js/test.js
@@ -1,2 +1,17 @@
-console.log(a)
-const a = 1;
+let str = "hello*3";
+let answer = "";
+let temp;
+const vowel = ['a', 'e', 'i', 'o', 'u']
+for (let i = 0; i < str.length; i++) {
+    temp = str[i].charCodeAt();
+    if (temp >= 97 && temp < 122) {
+        temp++;
+        if (vowel.includes(String.fromCharCode(temp))) {
+            temp -= 32;
+        }
+    } else if (temp === 122) {
+        temp = 65
+    }
+    answer += String.fromCharCode(temp);
+}
+console.log(answer)
