5ad45d6fc10cbe286104c1f1844f3b3a8f5266fb
youngmin <ymkwon3@gnu.ac.kr>
2022-07-09 15:29:35 +0900

    search-insert-position
    for loop

---
+++ js/leetcode_search-insert-position.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var searchInsert = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+      if(nums[i] >= target) {
+        return i;
+      }
+    }
+    return nums.length;
+};
+
+searchInsert([1, 3, 5, 6], 5)
